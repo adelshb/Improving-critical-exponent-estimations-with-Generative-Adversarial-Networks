bd1b7203869f1356f3ae0a204080865b6f4bb1cc
Ebi <h.dashti82@gmail.com>
2021-03-22 18:33:50 +0900

    Update slp.py

--- statphy/models/slp.py
+++ statphy/models/slp.py
@@ -16,17 +16,17 @@ def percolation_configuration(L, p):
     spin = (np.random.random(size=(L,L)) < p).astype(np.int8)
     return 2 * spin - 1
 
-def read_percolation_data(L, p_arr, pc, max_configs_per_p=2000):
-    X = []
-    y = []
+def read_percolation_data(L, p_arr, max_configs_per_p=1000):
+    X, y = [], []
+    unique_labels = []
+  
     j = 0
     for p in p_arr:
-        label = j if p != pc else 0
-        #label = 1 if p == pc else 0
+        unique_labels.append(j)
         for i in range(max_configs_per_p):
             X.append(percolation_configuration(L, p))
-            y.append(label)
+            y.append(j)
         j += 1
     X = np.array(X).reshape(-1, L, L, 1)
     y = np.array(y).reshape(-1, )
-    return X, y
+    return X, y, unique_labels
