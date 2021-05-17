f43b9788bcec2a2e7f809fbe4d5e58c57c831736
Matthieu Sarkis <matthieu.sarkis@gmail.com>
2021-05-17 18:07:49 +0900

    Update benchmark.py

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,3 +1,11 @@
 import tensorflow as tf
+from statphy.models.percolation import generate_data
 
 model = tf.keras.models.load_model('../saved_models/CNN_L128_N10000/saved-models.h5')
+
+X, _, _ = generate_data(L=128, 
+                        p_arr=[0.5928],
+                        max_configs_per_p=10)
+
+y_pred = model.predict(X)
+
