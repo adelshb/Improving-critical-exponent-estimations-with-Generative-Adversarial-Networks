05a38579c24d7faf75cf6da1d4bfe38979b054d8
Adel Sohbi <dlshb03@Adels-MacBook-Pro.local>
2021-05-20 10:26:00 +0900

    rebase from main

---
+++ src/GAN/utils.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+import tensorflow as tf
+
+import time
+import matplotlib.pyplot as plt
+
+import os
+
+def generator_loss(cross_entropy, fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+def discriminator_loss(cross_entropy, real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def train_step(images, generator, discriminator, generator_optimizer, discriminator_optimizer, cross_entropy, noise):
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+      generated_images = generator(noise, training=True)
+
+      real_output = discriminator(images, training=True)
+      fake_output = discriminator(generated_images, training=True)
+
+      gen_loss = generator_loss(cross_entropy, fake_output)
+      disc_loss = discriminator_loss(cross_entropy, real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+def generate_and_save_images(model, epoch, test_input):
+  predictions = model(test_input, training=False)
+
+  fig = plt.figure(figsize=(4, 4))
+
+  for i in range(predictions.shape[0]):
+      plt.subplot(4, 4, i+1)
+      plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+      plt.axis('off')
+
+  plt.savefig('./data/generated/image_at_epoch_{:04d}.png'.format(epoch))
+  #plt.show()
