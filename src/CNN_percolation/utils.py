8489d90d50e93008fce2e55a72c5aea6dd9b713b
Ebi <ebi@Ebi-16.local>
2021-05-23 01:31:29 +0900

    an update for cnn_percolation

--- src/CNN_percolation/utils.py
+++ src/CNN_percolation/utils.py
@@ -1,6 +1,6 @@
 
 import os
-import sys
+import json
 import pandas as pd
 
 
@@ -20,25 +20,16 @@ def write_numpy_dic_to_json(dic, path):
         df.to_json(f, indent=4, )
 
 
+def print_model_summary(model, path):
 
+    model.summary(print_fn=print)
 
-class Logger(object):
-    """
-    The following class prints the standard output to both console and file
-    """
-    def __init__(self, filename):
-        self.console = sys.stdout
-        self.file = open(filename, 'w')
- 
-    def write(self, message):
-        self.console.write(message)
-        self.file.write(message)
- 
-    def flush(self):
-        #self.console.flush()
-        #self.file.flush()
-        pass
-       
+    with open(make_path(path, 'model_summary.log'), 'w') as f:
+            model.summary(print_fn=lambda x: f.write(x + '\n'))
+        
+    # print optimizations
+    with open(make_path(path, 'optimizer.json'), 'w') as f:
+        json.dump(model.optimizer.get_config(), f, indent=4, sort_keys=True)
 
 
 
