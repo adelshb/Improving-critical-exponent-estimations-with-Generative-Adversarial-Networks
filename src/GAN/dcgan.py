correct sign activation function

src/GAN/dcgan.py
@@ -13,10 +13,11 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 
-def make_generator_model(noise_dimension=100):
+
+def make_generator_model():
     
     model = tf.keras.Sequential()
-    model.add(layers.Dense(2*2*256, use_bias=False, input_shape=(noise_dimension,)))
+    model.add(layers.Dense(2*2*256, use_bias=False, input_shape=(100,)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
@@ -73,13 +74,13 @@ def make_generator_model(noise_dimension=100):
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
     
-    model.add(layers.Conv2DTranspose(1, (3, 3), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.Conv2DTranspose(1, (3, 3), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
         
-    def sign_fct(x):
-        import tensorflow as tf
-        return tf.sign(x)
-        
-    model.add(layers.Lambda(lambda x: sign_fct(x)))
-    assert model.output_shape == (None, 128, 128, 1)
+    #def sign_fct(x):
+    #    import tensorflow as tf
+    #    return tf.sign(x)
+    #    
+    #model.add(layers.Lambda(lambda x: sign_fct(x)))
+    #assert model.output_shape == (None, 128, 128, 1)
 
     return model